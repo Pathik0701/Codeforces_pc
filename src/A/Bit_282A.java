d16db053cc781f5ef24b0944ab03825cb64196cb
Pathik0701 <pathikchauhan0701@gmail.com>
2026-07-28 01:05:45 +0530

    solution of 282A

---
+++ src/A/Bit_282A.java
@@ -0,0 +1,23 @@
+package A;
+
+import java.util.Scanner;
+
+public class Bit_282A{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = 0;
+
+        for(int i=0; i<n; i++){
+            String operation = sc.next();
+
+            if(operation.charAt(1) == '+'){
+                x++;
+            }else{
+                x--;
+            }
+        }
+        System.out.println(x);
+        sc.close();
+    }
+}
