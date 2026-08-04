2e7f25ed0e9a88962c8f0801f3fc8f8b93e18775
Pathik0701 <pathikchauhan0701@gmail.com>
2026-08-05 00:53:48 +0530

    solution of 263A

---
+++ src/A/BeautifulMatrix_263A.java
@@ -0,0 +1,26 @@
+package A;
+
+import java.util.Scanner;
+
+public class BeautifulMatrix_263A{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        int row = 0;
+        int col = 0;
+
+        for(int i=0; i<5; i++){
+            for(int j=0; j<5; j++){
+                int value = sc.nextInt();
+
+                if(value == 1){
+                    row = i;
+                    col = j;
+                    System.out.println( Math.abs(row - 2) + Math.abs(col - 2) );
+                }
+            }
+        }
+
+        sc.close();
+    }
+}
