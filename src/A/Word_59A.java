6fde6ff6651f7c52da4419339c46974188ec483d
Pathik0701 <pathikchauhan0701@gmail.com>
2026-08-21 03:58:34 +0530

    solution of 59A

---
+++ src/A/Word_59A.java
@@ -0,0 +1,31 @@
+package A;
+
+import java.util.Scanner;
+
+public class Word_59A{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+        int upper = 0;
+        int lower = 0;
+
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if(Character.isUpperCase(c)){
+                upper++;
+            }
+            else if(Character.isLowerCase(c)){
+                lower++;
+            }
+        }
+
+        if(upper>lower){
+            System.out.println(s.toUpperCase());
+        }else{
+            System.out.println(s.toLowerCase());
+        }
+
+        sc.close();
+    }
+}
