fcb57ec499047e06a98c6fe1cdbf34f81094b604
Pathik0701 <pathikchauhan0701@gmail.com>
2026-08-22 03:19:15 +0530

    solution of 236A

---
+++ src/A/BoyOrGirl_236A.java
@@ -0,0 +1,29 @@
+package A;
+
+import java.util.Scanner;
+
+public class BoyOrGirl_236A{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int[] seen = new int[26];
+        int distinct = 0;
+
+        for (char c : s.toCharArray()){
+            int index = c -'a';
+
+            if(seen[index]==0){
+                distinct++;
+                seen[index] = 1;
+            }
+        }
+
+        if(distinct % 2 ==0){
+            System.out.println("CHAT WITH HER!");
+        }
+        else{
+            System.out.println("IGNORE HIM!");
+        }
+        sc.close();
+    }
+}
