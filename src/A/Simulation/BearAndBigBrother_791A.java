37a63640470fd0d1ca48244256f7cd9a96d59ade
Pathik0701 <pathikchauhan0701@gmail.com>
2026-08-24 03:10:20 +0530

    solution of 791A

---
+++ src/A/Simulation/BearAndBigBrother_791A.java
@@ -0,0 +1,21 @@
+package A.Simulation;
+
+import java.util.Scanner;
+
+public class BearAndBigBrother_791A{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int count = 0;
+
+        while(a <= b){
+            a = a*3;
+            b = b*2;
+            count++;
+        }
+        System.out.println(count);
+
+        sc.close();
+    }
+}
