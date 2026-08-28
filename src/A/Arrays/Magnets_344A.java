462c53c863b0096f1f95acabb3ed96fd4505f6a6
Pathik0701 <pathikchauhan0701@gmail.com>
2026-08-29 01:38:32 +0530

    solution of 344A

---
+++ src/A/Arrays/Magnets_344A.java
@@ -0,0 +1,23 @@
+package A.Arrays;
+
+import java.util.*;
+
+public class Magnets_344A{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+        String previous = "";
+
+        for(int i=0; i<n; i++){
+            String current = sc.next();
+
+            if(!previous.equals(current)){
+                count++;
+            }
+            previous = current;
+        }
+        System.out.println(count);
+        sc.close();
+    }
+}
