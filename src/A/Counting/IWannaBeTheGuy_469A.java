6f6f16e8994c08876622e8996d9b66606a66dc0e
Pathik0701 <pathikchauhan0701@gmail.com>
2026-09-02 01:22:44 +0530

    solution of 469A

---
+++ src/A/Counting/IWannaBeTheGuy_469A.java
@@ -0,0 +1,40 @@
+package A.Counting;
+
+import java.util.*;
+public class IWannaBeTheGuy_469A{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean[] result = new boolean[n+1];
+
+        int p = sc.nextInt();
+        for(int i=0; i<p; i++){
+            int level = sc.nextInt();
+            result[level] = true;
+        }
+
+        int q = sc.nextInt();
+        for(int i=0; i<q; i++){
+            int level = sc.nextInt();
+            result[level] = true;
+        }
+
+        boolean possible = true;
+
+        for(int i=1; i<=n; i++){
+            if(result[i] == false){
+                possible = false;
+                break;
+            }
+        }
+
+        if(possible){
+            System.out.println("I become the guy.");
+        }
+        else{
+            System.out.println("Oh, my keyboard!");
+        }
+
+        sc.close();
+    }
+}
