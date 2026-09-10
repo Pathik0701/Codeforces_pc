db27e16ea4b8040d1b9f7a2dad941a4c73bd335c
Pathik0701 <pathikchauhan0701@gmail.com>
2026-09-11 03:22:45 +0530

    solution of 510A

---
+++ src/A/Implementation/FoxAndSnake_510A.java
@@ -0,0 +1,24 @@
+package A.Implementation;
+
+import java.util.*;
+
+public class FoxAndSnake_510A{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        for(int i=1; i<=n; i++){
+            if(i%2 == 1){
+                System.out.println("#".repeat(m));
+            }
+            else if(i%4 == 2){
+                System.out.println(".".repeat(m-1) + "#");
+            }
+            else{
+                System.out.println("#" + ".".repeat(m-1));
+            }
+        }
+        sc.close();
+    }
+}
