f5e849f3e4bee232f35442670071c3de39868733
Pathik0701 <pathikchauhan0701@gmail.com>
2026-09-21 23:44:19 +0530

    solution of 1335A

---
+++ src/A/Math/CandiesAndTwoSisters_1335A.java
@@ -0,0 +1,17 @@
+package A.Math;
+
+import java.util.Scanner;
+
+public class CandiesAndTwoSisters_1335A{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            long n = sc.nextLong();
+            System.out.println((n - 1) / 2);
+        }
+        sc.close();
+    }
+}
+
